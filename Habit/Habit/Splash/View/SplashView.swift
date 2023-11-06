4693968389bccb7fe533b5a73a254e2045df6435
IltonFerpe <129966216+IltonFerpe@users.noreply.github.com>
2023-11-06 18:19:24 -0300

    caso de erro na tela de login

--- Habit/Habit/Splash/View/SplashView.swift
+++ Habit/Habit/Splash/View/SplashView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct SplashView: View {
     
-    @State var state: SplashUiState = .loading
+    @State var state: SplashUiState = .goToSignInScreen
+    
     var body: some View {
         switch state {
         case .loading:
@@ -19,13 +20,13 @@ struct SplashView: View {
         case .goToHomeScreen:
             Text("Carregar tela principal")
         case .error(let msg):
-            Text("Mostrar erro: \(msg)")
+            loadingView(error: msg)
         }
     }
 }
 
 extension SplashView {
-  func loadingView() -> some View {
+  func loadingView(error: String? = nil) -> some View {
     ZStack {
       Image("logo")
         .resizable()
@@ -34,10 +35,21 @@ extension SplashView {
         .padding(20)
         .background(Color.white)
         .ignoresSafeArea()
+      
+      if let error = error {
+        Text("")
+          .alert(isPresented: .constant(true))  {
+            Alert(title: Text("Habit"), message: Text(error), dismissButton: .default(Text("Ok")) {
+              // faz algo quando some o alerta
+            })
+          }
+      }
     }
   }
 }
 
-#Preview {
-    SplashView()
+struct SplashView_Previews: PreviewProvider {
+  static var previews: some View {
+    SplashView(state: .error("Teste de erro no servidor"))
+  }
 }
